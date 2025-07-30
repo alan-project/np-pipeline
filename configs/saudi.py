3e49de9be459544a4c53447f2fe75ebce2c9978d
sangwoo kim <banana205@gmail.com>
2025-07-30 10:36:35 -0400

    updated article fetch ratio

--- configs/saudi.py
+++ configs/saudi.py
@@ -39,7 +39,7 @@ config = {
     "base_lang": "ar",
     "lang_list": ["ur", "hi", "bn", "en"],
     "select_all": False,
-    "top_article_ratio": 0.1,
+    "top_article_ratio": 0.07,
     "timezone": "Asia/Riyadh",
     "daily_popular_days": 2,
     "daily_popular_limit": 10,
